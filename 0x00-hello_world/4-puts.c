0193906713ab02af1edde7ce52c3f0af882bc33c
SHITTUGBENGA <psalmueltemmytorpe@gmail.com>
2022-07-02 15:32:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/** Main - Entry point 
+ *
+ * Description - C program that prints exactly
+ *  "Programming is like building a multilingual puzzle, 
+ *  followed by a new line."
+ *
+ *  Return - Always 0 (Success)
+ */
+
 int main(void)
 {
 
