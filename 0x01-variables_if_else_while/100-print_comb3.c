81f79cbfa8c80ed5189bbef7869d92483ecbf5c2
SHITTUGBENGA <psalmueltemmytorpe@gmail.com>
2022-07-04 22:23:15 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - a simple program that outputs 0-9 separated by commas
+ *
+ * Return: 0 on success
  */
-
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = '56'; i <= '57'; i++)
+	for (i = 48; i < 57; i++)
 	{
-		for (j = i + 1; j < '58'; j++)
+		for (j = i + 1; j < 58; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (i != '56' || j != '57')
+			if (i != 56 || j != 57)
 			{
 				putchar(',');
 				putchar(' ');
-
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
