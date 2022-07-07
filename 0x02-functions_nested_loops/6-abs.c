d2610a4a82c97dd9737eebffb17105f147553902
SHITTUGBENGA <user.SHITTUGBENGA>
2022-07-07 08:30:31 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point to check code
+ * @r - integer to be input(ed
+ * Description - This functions return the absolute value of a number
+ * Return: return absolute value of number /integer r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+
+}
