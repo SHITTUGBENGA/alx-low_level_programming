31887fbea5f85469eba4da41deadb117c81d9d63
SHITTUGBENGA <user.SHITTUGBENGA>
2022-07-07 01:55:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point to print_alphabet_x10 -
+ * Description: function uses 2-print_alphabet_x10 to print 
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+
+int main (void)
+{
+	void print_alphabet_x10(void);
+
+	return (0);
+}
