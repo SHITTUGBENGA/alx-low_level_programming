2dd2f3461742364ac8554f0152ab507ecf414654
SHITTUGBENGA <psalmueltemmytorpe@gmail.com>
2022-09-12 03:42:44 +0100

    Determining size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ *
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+
+}
