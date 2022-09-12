9900d95e858e92ffef7493ea42eaf24fbdb0f653
SHITTUGBENGA <psalmueltemmytorpe@gmail.com>
2022-09-12 04:54:43 +0100

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (success), 1 (otherwise)
+ *
+ */
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
